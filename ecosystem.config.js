ddc46a0e695d75c23b9cdad93bd260efb11965c2
哒哒ya <1179002658@qq.com>
2025-12-07 16:48:28 +0800

    fix:使用集群模式部署next.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,31 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "next-app",
+//       script: "npm",
+//       args: "start",
+//       cwd: "/app",
+//       instances: 1,
+//       exec_mode: "fork",
+//       env: {
+//         NODE_ENV: "production",
+//         PORT: 3000,
+//         HOSTNAME: "0.0.0.0",
+//       },
+//     },
+//   ],
+// };
 module.exports = {
   apps: [
     {
       name: "next-app",
-      script: "npm",
-      args: "start",
+      script: "./server.js",
       cwd: "/app",
-      instances: 1,
-      exec_mode: "fork",
+      instances: "max",
+      exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
         PORT: 3000,
-        HOSTNAME: "0.0.0.0",
       },
     },
   ],
